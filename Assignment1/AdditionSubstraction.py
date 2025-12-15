a8ba8553bf3c61ea3d2526c939020c90ff1265ea
Smruti Mishra <smrutisoumya.mishra@gmail.com>
2025-12-14 21:29:55 -0500

    Python program to add subtract, multiply and divide numbers

---
+++ Assignment1/AdditionSubstraction.py
@@ -0,0 +1,6 @@
+print('Hello!! Today we will add and subtract two numbers.')
+num1 = float(input('Please enter the first number : '))
+num2 = float(input('Now please enter the second number : '))
+
+print('The addition of', num1, 'and', num2, 'is :', num1 + num2)
+print('The subtraction of', num1, 'and', num2, 'is :', num1 - num2)
---
+++ Assignment1/MultiplicationDivision.py
@@ -0,0 +1,6 @@
+print('Hello!! Today we will multiply  and divide two numbers.')
+num1 = float(input('Please enter the first number : '))
+num2 = float(input('Now please enter the second number : '))
+
+print('The multiplication of', num1, 'and', num2, 'is :', num1*num2)
+print('The division of', num1, 'and', num2, 'is :', num1/num2)
