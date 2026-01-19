3903d900925f7cd07976da9ec21fd53938f85486
Smruti Mishra <smrutisoumya.mishra@gmail.com>
2026-01-18 23:08:03 -0500

    Updating logs

--- Assignment5/Rainfall.py
+++ Assignment5/Rainfall.py
@@ -16,11 +16,11 @@ total_months=0
 total_years= int(input("Enter number of years: "))
 for year in range(total_years):
       for i in month:
-        rainfall = float(input(f"Enter rainfall for {month[i]} for year {year+1}: "))
+        rainfall = float(input(f"Enter rainfall for {month[i]} for year {year+1} in inches: "))
         total_rainfall += rainfall
         total_months += 1
 
 average_rainfall = total_rainfall / total_months
 print(f"\nNumber of months: {total_months}")
-print(f"Total rainfall: {total_rainfall:.2f}")
-print(f"Average rainfall per month: {average_rainfall:.2f}")
+print(f"Total inches of rainfall: {total_rainfall:.2f}")
+print(f"Average rainfall per month: {average_rainfall:.2f} inches")
